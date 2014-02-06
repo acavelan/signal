80a67cab534c3098d330176e532e6d92ae84ca1e
razlock <razlock@gmail.com>
2014-02-06 14:58:44 +0100

    add inline

--- Signal.hpp
+++ Signal.hpp
@@ -15,30 +15,30 @@ class Signal
             _calls.clear();
         }
 
-        void connect(std::function<void(Args...)> fct)
+        inline void connect(std::function<void(Args...)> fct)
         {
             _calls.push_back(fct);
         }
         
         template<typename Object>
-        void connect(Object* object, void (Object::*method)(Args...))
+        inline void connect(Object* object, void (Object::*method)(Args...))
         {
             _calls.push_back([object,method](Args... args){(*object.*method)(args...);});
         }
         
         template<typename Object>
-        void connect(Object* object, void (Object::*method)(Args...) const)
+        inline void connect(Object* object, void (Object::*method)(Args...) const)
         {
             _calls.push_back([object,method](Args... args){(*object.*method)(args...);});
         }
         
         template<typename Object>
-        void connect(const Object* object, void (Object::*method)(Args...) const)
+        inline void connect(const Object* object, void (Object::*method)(Args...) const)
         {
             _calls.push_back([object,method](Args... args){(*object.*method)(args...);});
         }
 
-        void emit(Args... args)
+        inline void emit(Args... args)
         {
             for(auto call : _calls)
                 call(args...);
